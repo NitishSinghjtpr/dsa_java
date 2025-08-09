51dc5d271f49093a84881517bcb243cfa9ae7490
Nitish Kumar <your_email@example.com>
2025-08-09 09:32:01 +0530

    update code

--- BitManipulation/chessGame.java
+++ BitManipulation/chessGame.java
@@ -1,11 +1,15 @@
 public class chessGame {
-    public static int countHome(int a,int b){
+   public static int countHome(int a,int b){
         int count=0;
         count+=Math.min(8-a,8-b);
-        count+=Math.min(8-a,8-b);
-        count+=Math.min(8-a,8-b);
-        count+=Math.min(8-a,8-b);
+        count+=Math.min(8-a,b-1);
+        count+=Math.min(a-1,8-b);
+        count+=Math.min(a-1,b-1);
         return count;
+    }
+    public static void main(String[] args) {
+        int ans=countHome(5, 01);
+        System.out.println(ans);
     }
     
     
