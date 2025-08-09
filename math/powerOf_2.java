d317f549e1cae66feb5ecacaac9c900508f044da
Nitish Kumar <your_email@example.com>
2025-08-09 09:38:41 +0530

    add

---
+++ math/powerOf_2.java
@@ -0,0 +1,23 @@
+package math;
+
+
+    public class powerOf_2 {
+    static int powerOf(int x){
+        while (x!=1) {
+            if (x%2==1) {
+                return 0;
+               
+            }else{
+                x/=2;
+            }
+        }
+        return 1;
+    }
+    public static void main(String[] args) {
+       int ans= powerOf(64);
+        System.out.println(ans);
+
+    }
+    
+}
+
