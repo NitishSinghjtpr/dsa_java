bc6098f1b8a2c6d171e6de5dca67f25176b4a800
Nitish Kumar <your_email@example.com>
2025-08-11 13:01:10 +0530

    add code

--- Array/Second Largest/secLarge.java
+++ Array/Second Largest/secLarge.java
@@ -1,19 +1,20 @@
 public class secLarge {
     public static void main(String[] args) {
-        int arr[]={4,2,5,10,3,9,7,1,6};
-        int second = Integer.MIN_VALUE,max=0;;
+        int arr[]={10,10,10};
+        int second = -1,max=-1;;
         for(int i=0;i<arr.length;i++){
             if (max<arr[i]) {
                 max=arr[i];
             }
         }
-        System.out.println(max);
+        System.out.println(max);//for max
+        //jab sara element equall rahega to return -1
         for(int i=0;i<arr.length;i++){
             if (max!=arr[i]&&second<arr[i]) {
                 second=arr[i];
             }
         }
-        System.out.println(second);
+        System.out.println(second);//for second max
     }
     
 }
