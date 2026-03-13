f64cc27cbe7121c45842e1bf17c991d797252d82
Nitish Kumar <nitishsinghjtpr@gmail.com>
2026-03-13 15:00:16 +0530

    update max value

--- Array/Find Max Element/maxElement.java
+++ Array/Find Max Element/maxElement.java
@@ -1,7 +1,7 @@
 public class maxElement {
     public static void main(String[] args) {
         int arr[]={2,31,5,1,4,11};
-        int max=0;
+        int max=arr[0];
         for(int i=0;i<arr.length;i++){
            
           if(max<arr[i]){
