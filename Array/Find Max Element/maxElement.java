8aed0516aa2d48d7b2ad1501c6e204a54271e5d4
Nitish Kumar <nitishsinghjtpr@gmail.com>
2026-03-13 14:57:39 +0530

    max Element

---
+++ Array/Find Max Element/maxElement.java
@@ -0,0 +1,15 @@
+public class maxElement {
+    public static void main(String[] args) {
+        int arr[]={2,31,5,1,4,11};
+        int max=0;
+        for(int i=0;i<arr.length;i++){
+           
+          if(max<arr[i]){
+              max=arr[i];
+              
+          }
+           
+        }
+        System.out.println(max);
+    }
+}
