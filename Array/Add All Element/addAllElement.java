3ae600e62c9ccad3334ba38cc94e2d1808376d01
Nitish Kumar <nitishsinghjtpr@gmail.com>
2026-03-13 14:50:55 +0530

    Add All Element

---
+++ Array/Add All Element/addAllElement.java
@@ -0,0 +1,12 @@
+public class addAllElement {
+    public static void main(String[] args) {
+        int arr[]={2,3,5};
+        int ans=0;
+        for(int i=0;i<arr.length;i++){
+           
+           ans+=arr[i];
+           
+        }
+        System.out.println(ans);
+    }
+}
