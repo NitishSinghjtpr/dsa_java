a2c65583321f56d1aaebcc0a63656ed746d0ee4e
Nitish Kumar <nitishsinghjtpr@gmail.com>
2026-03-13 14:36:22 +0530

    Find negetive number

---
+++ Array/Find Negetive number/findnegeyive.java
@@ -0,0 +1,10 @@
+public class findnegeyive {
+    public static void main(String[] args) {
+        int arr[]={2,5,-4,6,-7,3,-1};
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<0){
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+}
